0989d98732bc8908c8d1b7fbfe42f362061ef8fe
yurilopesm <yurilopesm@hotmail.com>
2023-04-30 15:54:22 -0300

    checkout page change

--- src/pages/CheckoutPage/index.js
+++ src/pages/CheckoutPage/index.js
@@ -14,8 +14,26 @@ import {
 import thebookhouse from "../../assets/images/thebookhouse.png";
 import book1 from "../../assets/images/book1.jpeg";
 import { Link } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "../../context/AuthContext";
+import axios from "axios";
 
 export default function CheckoutPage() {
+  const [cartList, setCartList] = useState([])
+  const { token, userName } = useContext(AuthContext);
+  const {total, setTotal} = useState(0)
+  const config = {
+    headers:
+      { Authorization: `Bearer ${token}` }
+  };
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API_URL}/cart`, config)
+      .then((res) => {
+        setCartList(res.data)
+      })
+      .catch((err) => alert(err.message))
+  }, [])
+
   return (
     <CheckoutContainer>
       <Header>
@@ -25,30 +43,42 @@ export default function CheckoutPage() {
       </Header>
 
       <h1>Meu Carrinho</h1>
-      <CartContainer>
-        <img src={book1} alt="book"></img>
-        <h2>
-          Python Programming: 3 books in 1 - Ultimate Beginner's, Intermediate &
-          Advanced Guide to Learn Python Step by Step
-        </h2>
-        <h3>R$ 145,00</h3>
-      </CartContainer>
+      {cartList.map((book, index) => {
+        const { image, title, price } = book
+        return (
+          <CartContainer key={index}>
+            <img src={image} alt="book"></img>
+            <h2>
+              {title}
+            </h2>
+            <h3>R$ {price}</h3>
+          </CartContainer>
+        )
+      })}
 
       <OrderedContainer>
         <h1>Resumo do Pedido</h1>
         <OrderSummary>
           <div>
-            <Product>1 produto</Product>
+            {cartList.map(book => {
+              return (
+                  <Product>{book.title}</Product>
+              )
+            })}
             <Total>Total</Total>
           </div>
 
           <div>
-            <Price>R$ 145,00</Price>
-            <TotalPrice>R$ 145,00</TotalPrice>
+            {cartList.map(book => {
+              return (
+                <Price>R$ {book.price}</Price>
+              )
+            })}
+            <TotalPrice>R$ {total}</TotalPrice>
           </div>
         </OrderSummary>
         <Divider />
-        <CheckoutButton>Finalizar Pedido</CheckoutButton>
+        <CheckoutButton >Finalizar Pedido</CheckoutButton>
       </OrderedContainer>
     </CheckoutContainer>
   );
